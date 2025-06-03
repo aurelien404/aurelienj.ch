0184b7db68d480b8a13f1e6624fa633b94b8f1e2
aurelienj <contact@aurelienj.ch>
2025-06-03 12:52:11 +0200

    Site perso V1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,77 @@
-import { Link } from 'react-router-dom'; 
+import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
     
 function Header() {
-    const { t } = useTranslation();
+    const [isShrunk, setIsShrunk] = useState(false);
+    const { t, i18n } = useTranslation();
+    const location = useLocation();
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsShrunk(true);
+            } else {
+                setIsShrunk(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    };
+
     return (
-        <header className="w-screen h-[100px] p-4 font-extrabold">
-            <p className='text-3xl '>aurelienj.</p>
-            <nav className="pl-4">
-                <Link to="/" className="mr-4">Home</Link>
-                <Link to="/service" className="mr-4">Service</Link>
-                <Link to="/contact">Contact</Link>
+        <header className={`${isShrunk ? 'py-3 px-7' : 'py-5 px-10'} sticky top-0 left-0 w-full flex justify-between items-center bg-zzcontrast border-b-2 border-base transition-all duration-300 ease-out z-10`}>
+            <p className={`${isShrunk ? 'text-2xl' : 'text-3xl'} font-extrabold transition-all duration-300 ease-out`}>aurelienj.</p>
+            <span className='flex justify-between'>
+                <nav className={`${isShrunk ? 'text-base' : 'text-xl'} font-bold transition-all duration-300 ease-out mr-4`}>
+                    <Link to="/" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className={`mr-4 link-style ${location.pathname === '/' ? 'link-style-active' : ''}`}>{t('link.home')}</Link>
+                    <Link to="/service" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className={`mr-4 link-style ${location.pathname === '/service' ? 'link-style-active' : ''}`}>{t('link.service')}</Link>
+                    <Link to="/contact" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className={`mr-4 link-style ${location.pathname === '/contact' ? 'link-style-active' : ''}`}>{t('link.contact')}</Link>
             </nav>
+                
+                <button
+                    className={`font-extrabold mx-0.5 border-none bg-none cursor-pointer ${i18n.language === 'fr' ? 'text-zzlink text-sm' : ''}`}
+                    onClick={() => changeLanguage('fr')}>FR</button>
+                <button
+                    className={`font-extrabold mx-0.5 border-none bg-none cursor-pointer ${i18n.language !== 'fr' ? 'text-zzlink text-sm' : ''}`}
+                    onClick={() => changeLanguage('en')}>EN</button>
+            </span>
         </header>
     );
 }
 
 export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
