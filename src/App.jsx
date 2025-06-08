d9fd7c82b156926693516b71561d7763e8547812
aurelienj <contact@aurelienj.ch>
2025-06-08 11:38:37 +0200

    Install i18N

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useTranslation } from 'react-i18next';
+
 function App() {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <>
       <h1 className='font-extrabold ' >HELLO!</h1>
+      <div>
+        <h1>{t('welcome')}</h1>
+        <p>{t('description')}</p>
+
+        <button onClick={() => changeLanguage('en')}>EN</button>
+        <button onClick={() => changeLanguage('fr')}>FR</button>
+      </div>
     </>
   )
 };
