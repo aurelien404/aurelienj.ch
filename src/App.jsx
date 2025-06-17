e649f469a116499587c5440dafa78c56f751b334
aurelienj <contact@aurelienj.ch>
2025-06-17 19:32:13 +0200

    Removed maintenance page

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Maintenance />} />
+        <Route path="/" element={<Home />} />
         <Route path="/Projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,35 +1,38 @@
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 function Home() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <>
-            <div className="w-full h-[80vh] flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between px-6 md:px-20 py-20 border-b-2">
-                <div className="w-full md:w-3/5 h-full flex flex-col justify-center gap-6">
-                    <h1 className="text-3xl md:text-6xl font-extrabold text-zzbase">
-                        {t('home.herotitre')}
-                    </h1>
-                    <p className="text-lg text-zzbase max-w-xl" dangerouslySetInnerHTML={{ __html: t('home.herotext') }}></p>
+  return (
+    <>
+      <div className="w-full h-[80vh] flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between px-6 md:px-20 py-20">
+        <div className="w-full md:w-3/5 h-full flex flex-col justify-center gap-6">
+          <h1 className="text-3xl md:text-5xl font-extrabold text-zzbase">
+            {t("home.herotitre")}
+          </h1>
+          <p
+            className="text-lg text-zzbase max-w-xl"
+            dangerouslySetInnerHTML={{ __html: t("home.herotext") }}
+          ></p>
 
-                    <Link to="/contact" onClick={() => { window.scrollTo({ top: 0, behavior: 'smooth' }) }}
-                        className="w-fit mx-auto md:mx-0 mt-4 border-2 py-4 px-3 md:py-2 border-zzbase text-zzbase transform transition duration-200 hover:scale-105 active:scale-95
+          <Link
+            to="/contact"
+            onClick={() => {
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
+            className="w-fit mx-auto md:mx-0 mt-4 border-2 py-4 px-3 md:py-2 border-zzbase text-zzbase transform transition duration-200 hover:scale-105 active:scale-95
                         hover:border-zzlink focus:border-zzlink active:border-zzlink
                         hover:text-zzlink focus:text-zzlink active:text-zzlink"
-                    >
-                        {t('home.btn')}
-                    </Link>
+          >
+            {t("home.herobtn")}
+          </Link>
+        </div>
 
-                </div>
-
-                <div className="hidden md:block w-2/5 h-full bg-zzlink">
-                </div>
-            </div>
-            < div className='w-full h-[1000px] '>
-            </div>
-        </>
-    );
+        <div className="hidden md:block w-2/5 h-full bg-zzlink"></div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
