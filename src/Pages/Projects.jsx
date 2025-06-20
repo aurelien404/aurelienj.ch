7d93c5e38e8e9fc336434fc55fee1b28165f73db
aurelienj <contact@aurelienj.ch>
2025-06-20 18:34:23 +0200

    Adding Memory game on projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -2,12 +2,13 @@ import { useTranslation } from "react-i18next";
 import Todolist from "../components/projects/toDoList";
 import AuthApp from "../components/projects/auth";
 import InputApp from "../components/projects/input";
+import MemoryApp from "../components/projects/memory";
 
 function UiUxLab() {
   const { t } = useTranslation();
   return (
     <>
-      <div className="absolute top-25 left-50 w-[250px] -z-1 h-70 block md:hidden bg-zzlink"></div>
+      <div className="absolute top-25 left-50 w-45 md:w-[250px] -z-1 h-70 block md:hidden bg-zzlink"></div>
       <div className="w-full px-[3vw] pt-10 md:pt-30 md:px-[15vw] ">
         <h1 className="text-3xl md:text-6xl font-extrabold text-zzbase pb-5">
           {t("UiUxLab.title")}
@@ -20,6 +21,22 @@ function UiUxLab() {
         </p>
 
         <div className="w-full">
+          <div className="w-full max-w-[600px] mx-auto border-b-2 mt-5">
+            <MemoryApp />
+            <div className="w-full text-center py-5">
+              <h2 className="text-xl md:text-3xl font-extrabold text-zzbase">
+                {t("UiUxLab.p41")}
+              </h2>
+              <p className="text-sm md:text-base font-light text-zzbase">
+                {t("UiUxLab.p42")}
+              </p>
+              <span className="w-4/6 md:w-2/6 inline-flex justify-around py-3 text-xs md:text-sm font-medium text-zzlink">
+                <p>React</p>
+                <p>Tailwind</p>
+              </span>
+            </div>
+          </div>
+
           <div className="w-full max-w-[600px] mx-auto border-b-2 mt-5">
             <Todolist />
             <div className="w-full text-center py-5">
